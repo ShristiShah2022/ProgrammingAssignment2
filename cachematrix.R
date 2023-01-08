12c0ce9e616964c2a2172778aa12f55dabc6b7d1
ShristiShah2022 <shristi.cse.imps@gmail.com>
2023-01-08 12:09:40 +0530

    adding function logic for caching inverse of matrix and if present fetch from cache instead of re computation

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invMatrix <- NULL
+  set <- function(y){
+     x <<- y
+     invMatrix <<- NULL
+    
+  }
+  get <- function()x
+  setInvMatrix <- function (inverse) i<<- inverse
+  getInvMatrix <- function() i
+  list (set = set, get=get, 
+        setInvMatrix = setInvMatrix, getInvMatrix= getInvMatrix)
 }
 
 
@@ -12,4 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getInvMatrix()
+  if(!is.null(i)){
+    message("Get the cached data")
+    return (i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInvMatrix(i)
+  i
+  
 }
